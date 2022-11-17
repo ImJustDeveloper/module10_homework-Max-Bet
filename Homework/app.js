54a58f3b21582e3896bdfda2f5d3f06526742441
ImJustDeveloper <116288506+ImJustDeveloper@users.noreply.github.com>
2022-11-17 23:29:07 +0300

    Add files via upload

---
+++ Homework/app.js
@@ -0,0 +1,97 @@
+
+//Task 1
+
+// let x = prompt("Введите число");
+//
+// if ((typeof x) == "number"){
+//     x % 2 ==0
+//     if((x % 2 ==0)== true){
+//         console.log("Вы ввели четное число")
+//     } else if((x % 2 ==0)== false){
+//         console.log("Вы ввели нечетное число")
+//     }
+// }
+//
+// else if((typeof x) == "NaN"){
+//     console.log("Кажется, вы ошиблись")
+// }
+
+//Task 2
+
+// let a = 1;
+//
+// if((typeof a) === "number"){
+//     console.log("it's a number")
+// } else if((typeof a) === "string"){
+//     console.log("it's a string")
+// } else if((typeof a) === "boolean"){
+//     console.log("it's a boolean")
+// } else {
+//     console.log("Not defined")
+// }
+
+
+// Task 3
+
+// let e;
+
+// console.log(Math.random(100, 200));
+
+// Task 4
+
+// let str = "Hallo";
+
+// console.log(str.split('').reverse().join(''));
+
+// Task 5
+
+// let max = [1, 2, 3, 'hello', true];
+//
+// console.log(max.length);
+//
+// let result = max.map(function(item, index, array) {
+//
+//     return item;
+//
+// });
+//
+// console.log(result);
+
+//Task 6
+// for(let i = 0; i < 4; i = i + 1) {
+//     if(typeof arr[i] === typeof arr[i+1]){
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+//Task 7
+
+// let arr = [1, 2, 3, 4];
+//
+// for (let i = 0; i < 5; i++){
+//     if((arr[i] % 2 === 0) === true){
+//         console.log(i)
+//     } }
+//
+// for (let i = 0; i < 5; i++){
+//     if((arr[i] % 2 === 0) === false){
+//         console.log(i)
+//     } }
+
+//Task 8
+
+// let myMap = new Map();
+//
+// myMap.set(1, "you");
+// myMap.set(2, "are");
+// myMap.set(3, "badass");
+//
+// for (let keys of myMap.keys()){
+//     console.log("Ключ: " + keys)
+// }
+//
+// for (let values of myMap.values()){
+//     console.log("Значения: " + values)
+// }
